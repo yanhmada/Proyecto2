5fc60f3ced9a9c773a7e665bc57e5fb8281005ac
yanhmada <98841526+yanhmada@users.noreply.github.com>
2022-03-24 23:49:48 -0700

    Update scrcovs.sh

--- scrcovs.sh
+++ scrcovs.sh
@@ -7,7 +7,7 @@ curl -L -O https://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/histori
 unzip datos_abiertos_covid19_22.03.2022.zip
 
 #cortar datos y guardar en nuevo csv
-csvcut -c 36,8,6,9,13,16,18 220322COVID19MEXICO.csv > datoscov.csv &&\
+csvcut -c 36,8,6,9,13,16,18 220322COVID19MEXICO.csv > datoscov.csv 
 #Filtrar, solo casos confirmados
 csvgrep -c 1 -r "[123]" datoscov.csv  > filtrado.csv |csvgrep -c ENTIDAD_RES -m "26" 
 echo file is now processed
